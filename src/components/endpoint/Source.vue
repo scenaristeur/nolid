b933a2275d6eccdae1f8ee7f4ff7ba3c004a4d0d
Scenaristeur <scenaristeur@gmail.com>
2021-10-18 11:08:23 +0200

    init

---
+++ src/components/endpoint/Source.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-container>
+    <b-input-group size="sm" prepend="Endpoint">
+      <b-form-input v-model="endpoint"></b-form-input>
+    </b-input-group>
+    <b-input-group size="sm" prepend="Dataset">
+      <b-form-input v-model="dataset"></b-form-input>
+    </b-input-group>
+      <b-button variant="outline-primary" @click="load">Load</b-button>
+
+
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "Source",
+  data(){
+    return {
+      endpoint: "http://localhost:3030",
+      dataset: "pizza"
+    }
+  },
+  methods:{
+    load(){
+      let url = this.endpoint+'/'+this.dataset
+      console.log(url)
+      this.$store.dispatch('endpointTest', url)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,71 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "axios"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    source: {
+      endpoint: "",
+      datasets: "",
+      query_url: ""
+    },
+    sources: {},
+    data: {}
   },
   mutations: {
+    setData(state, data){
+      state.data = data
+      console.log(state.data)
+    }
   },
   actions: {
-  },
-  modules: {
-  }
-})
+    endpointTest(context, url){
+      let query = `SELECT ?subject ?predicate ?object
+      WHERE {
+        ?subject ?predicate ?object
+      }
+      LIMIT 25`
+      axios(
+        {
+          method: 'post',
+          url: url+'/query?query='+query,
+          headers: {
+            //'Content-type': 'application/json',
+            'Accept': 'application/sparql-results+json'
+          },
+          //  query: query,
+          // headers: {
+          //   'Content-Type': 'application/json',
+          // }
+          //responseType: 'application/sparql-results+json'
+        })
+        .then(function (response) {
+          console.log(response);
+          context.commit('setData', response)
+        })
+        .catch(function (error) {
+          console.log(error);
+          context.commit('setData', error)
+        });
+      },
+      minimalEndpointTest(context, url){
+        axios(
+          {
+            method: 'get',
+            url: url,
+          })
+          .then(function (response) {
+            console.log(response);
+            context.commit('setData', response)
+          })
+          .catch(function (error) {
+            console.log(error);
+            context.commit('setData', error)
+          });
+        }
+      },
+      modules: {
+      }
+    })
