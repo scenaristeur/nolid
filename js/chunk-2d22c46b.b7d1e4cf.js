cbe78c1e1b3297445fc3153d7c92f479f0054dee
Scenaristeur <scenaristeur@gmail.com>
2021-10-19 09:44:23 +0200

    review

---
+++ js/chunk-2d22c46b.b7d1e4cf.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22c46b"],{f319:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("b-button",{on:{click:e.editFragment}},[e._v("Nouveau")])},a=[],c={name:"Nouveau",methods:{editFragment:function(){this.$editFragment()}}},o=c,i=t("2877"),r=Object(i["a"])(o,u,a,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d22c46b.b7d1e4cf.js.map
---
+++ precache-manifest.8cfc9e571f83f5e539752ed36b261e5b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef7aac719e7e8953760",
+    "url": "/nolid/css/app.abbdcc0c.css"
+  },
+  {
+    "revision": "971ee94206580dcda45b",
+    "url": "/nolid/css/chunk-vendors.85f48eb3.css"
+  },
+  {
+    "revision": "966c63c01545ddc61b4201498dc01835",
+    "url": "/nolid/index.html"
+  },
+  {
+    "revision": "425f19a09adb1d91c6c4",
+    "url": "/nolid/js/about.8c0caecf.js"
+  },
+  {
+    "revision": "cef7aac719e7e8953760",
+    "url": "/nolid/js/app.556549b3.js"
+  },
+  {
+    "revision": "013f95821d9c56b93196",
+    "url": "/nolid/js/chunk-2d21088a.99612842.js"
+  },
+  {
+    "revision": "23b1fb7837d2b35dc0f7",
+    "url": "/nolid/js/chunk-2d22c46b.b7d1e4cf.js"
+  },
+  {
+    "revision": "971ee94206580dcda45b",
+    "url": "/nolid/js/chunk-vendors.6949dc3f.js"
+  },
+  {
+    "revision": "9f3c5384063799737d560240d8d792a6",
+    "url": "/nolid/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nolid/robots.txt"
+  }
+]);
